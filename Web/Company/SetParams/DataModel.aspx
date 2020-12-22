3c72da5052fa2ee94f11dac705151a071637a5b7
niels <892182825@qq.com>
2020-12-23 03:00:57 +0800

    弄了个数据模型，方便老板跑数据看结果定参数 修改商业计划文档

---
+++ Web/Company/SetParams/DataModel.aspx
@@ -0,0 +1,115 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="DataModel.aspx.cs" Inherits="Company_SetParams_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=yes" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+    <meta name="format-detection" content="telephone=no" />
+    <title>数据模型</title>
+    <link href="../../CSS/bootstrap-cerulean.min.css" rel="stylesheet" />
+    <link href="../../CSS/bootstrap.css" rel="stylesheet" />
+    <script src="../bower_components/jquery/jquery.min.js"></script>
+    <script src="../../JS/bootstrap.js"></script>
+    <script>
+        $(function () {
+          
+            $("#sctl").click(function () {
+                var ck ="sctl";
+                $("#HiddenField1").val(ck);
+
+            });
+            $("#rstl").click(function () {
+                var ck = "rstl";
+                $("#HiddenField1").val(ck);
+
+            });
+            $("#cjjs").click(function () {
+                var ck = "cjjs";
+                $("#HiddenField1").val(ck);
+
+            });
+            $("#mnyx").click(function () {
+                var ck = "mnyx";
+                $("#HiddenField1").val(ck);
+
+            });
+
+        });
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <div class="view">
+							<div class="tabbable" id="tabs-45854">
+								<!-- Only required for left/right tabs -->
+								<ul class="nav nav-tabs">
+									<li class="active" id="sctl" runat="server">
+										<a href="#panel-625832" data-toggle="tab" >市场体量</a>
+									</li>
+									<li id="rstl">
+										<a href="#panel-677389" data-toggle="tab" >人数体量</a>
+									</li>
+                                    <li id="cjjs">
+										<a href="#panel-665412" data-toggle="tab" >未定</a>
+									</li>
+                                    <li id="mnyx">
+										<a href="#panel-665444" data-toggle="tab" >未定</a>
+									</li>
+								</ul>
+								<div class="tab-content">
+                                    <div class="tab-pane active" id="panel-625832">
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1">入金(U)</label>
+                                            <asp:TextBox runat="server" ID="rj" class="form-control" placeholder="入金"></asp:TextBox>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1">每日递增(%)</label>
+                                            <asp:TextBox runat="server" ID="dz" class="form-control" placeholder="每日递增"></asp:TextBox>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1">时间（天）</label>
+                                            <asp:TextBox runat="server" ID="shijian" class="form-control" placeholder="时间"></asp:TextBox>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1">每日卖出（每天入金的%）</label>
+                                            <asp:TextBox runat="server" ID="maichu" class="form-control" placeholder="每日卖出"></asp:TextBox>
+                                        </div>
+                                    </div>
+									<div class="tab-pane" id="panel-677389">
+										<div class="form-group">
+                                            <label for="exampleInputEmail1">衡定矿机等级(U)</label>
+                                            <asp:TextBox runat="server" ID="TextBox1" class="form-control" placeholder="衡定矿机等级"></asp:TextBox>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1">初始人数</label>
+                                            <asp:TextBox runat="server" ID="TextBox2" class="form-control" placeholder="初始人数"></asp:TextBox>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1">时间（天）</label>
+                                            <asp:TextBox runat="server" ID="TextBox3" class="form-control" placeholder="时间"></asp:TextBox>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1">人数递增（%）</label>
+                                            <asp:TextBox runat="server" ID="TextBox4" class="form-control" placeholder="人数递增"></asp:TextBox>
+                                        </div>
+									</div>
+                                    <div class="tab-pane" id="panel-665412">
+										其他数据模型看需要哪个再商量添加
+									</div>
+								</div>
+							</div>
+						</div>
+            <asp:Button runat="server" ID="btn" CssClass="btn btn-default" OnClick="btn_Click" Text="提交" />
+            <asp:HiddenField ID="HiddenField1" runat="server" Value="sctl" />
+            <div id="modelshow" runat="server" class="view">
+             
+        </div>  
+        </div>
+    </form>
+</body>
+</html>
---
+++ Web/Company/SetParams/DataModel.aspx.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Company_SetParams_Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btn_Click(object sender, EventArgs e)
+    {
+        string html = @" <ol>";
+        if (HiddenField1.Value == "sctl")
+        { 
+        if (rj.Text == "" || dz.Text == "" || shijian.Text == "")
+        {
+            ClientScript.RegisterStartupScript(this.GetType(), "", "<script>alert('请填满参数！');</script>", false);
+            return;
+        }
+        else
+        {
+            
+            double rujin =Convert.ToDouble(rj.Text);
+            double dizeng = Convert.ToDouble(dz.Text);
+            double date = Convert.ToDouble(shijian.Text);
+            double maichuU = Convert.ToDouble(maichu.Text);
+            double X = 0;
+            double num = 0;
+            double num1 = 0;
+            for (int a = 0; a < date; a = a + 1)
+            {
+                if (a == 0)
+                {
+                    num += rujin;
+                    num1 += rujin;
+                    X = rujin;
+                }
+                else
+                {
+                    num1 += (num * maichuU / 100);
+                    num +=X+( X * dizeng/100);
+                   
+                    X =X+(X * dizeng/100);
+                }
+                
+            }
+            
+            html += @"  <li  > 总入金获得U："+ (num+num*0.05+num1*0.2).ToString("0.0000") + " </li>";
+            html += @"  <li  > 用户卖出U：" + num1.ToString("0.0000") + " </li>";
+            html += @"  <li  > 入金燃烧5%E获得U：" + (num * 0.05).ToString("0.0000") + " </li>";
+            html += @"  <li  > 用户出金燃烧20%E获得U：" + (num1 * 0.2).ToString("0.0000") + " </li>";
+            html += @"  <li  > 去掉用户卖出获得U：" + ((num + num * 0.05 + num1 * 0.2) - num1).ToString("0.0000") + " </li>";
+            html += @"  <li  > 备注：用户卖出U的计算没办法考虑用户是否出局，所以仅供参考。 </li>";
+            html += " </ol>";
+            modelshow.InnerHtml = html;
+            rj.Text = ""; 
+            dz.Text = ""; 
+            shijian.Text = "";
+            maichu.Text = "";
+            //ClientScript.RegisterStartupScript(this.GetType(), "", "<script>alert('总共获得:"+num.ToString("0.0000")+"USDT！');</script>", false);
+            //return;
+        }
+        }
+
+        if (HiddenField1.Value == "rstl")
+        {
+            if (TextBox2.Text == "" || TextBox4.Text == "" || TextBox3.Text == "")
+            {
+                ClientScript.RegisterStartupScript(this.GetType(), "", "<script>alert('人数、递增、时间必填，衡定矿机等级默认0U！');</script>", false);
+                return;
+            }
+            else
+            {
+                if (TextBox1.Text == "")
+                {
+                    TextBox1.Text = "0";
+                }
+                double dj = Convert.ToDouble(TextBox1.Text);
+                double rujin = Convert.ToDouble(TextBox2.Text);
+                double dizeng = Convert.ToDouble(TextBox4.Text);
+                double date = Convert.ToDouble(TextBox3.Text);
+               
+                double X = 0;
+                double num = 0;//人数
+                double num1 = 0;//体量
+                for (int a = 0; a < date; a = a + 1)
+                {
+                    if (a == 0)
+                    {
+                        num += rujin;
+                       
+                        X = rujin;
+                    }
+                    else
+                    {
+                      
+                        num += X + (X * dizeng / 100);
+
+                        X = X + (X * dizeng / 100);
+                    }
+
+                }
+                num1 = num * dj;
+
+                html += @"  <li  > 总入金获得U：" + num1.ToString("0.0000") + " </li>";
+                html += @"  <li  > 入金燃烧5%E获得U：" + (num1 * 0.05).ToString("0.0000") + " </li>";
+                html += @"  <li  > 总注册人数：" + num.ToString("0.0000") + " </li>";
+               
+                html += " </ol>";
+                modelshow.InnerHtml = html;
+                TextBox1.Text = "";
+                TextBox2.Text = "";
+                TextBox4.Text = "";
+                TextBox3.Text = "";
+                //ClientScript.RegisterStartupScript(this.GetType(), "", "<script>alert('总共获得:"+num.ToString("0.0000")+"USDT！');</script>", false);
+                //return;
+            }
+        }
+
+        if (HiddenField1.Value == "cjjs")
+        {
+            if (rj.Text == "" || dz.Text == "" || shijian.Text == "")
+            {
+                ClientScript.RegisterStartupScript(this.GetType(), "", "<script>alert('请填满参数！');</script>", false);
+                return;
+            }
+            else
+            {
+
+                double rujin = Convert.ToDouble(rj.Text);
+                double dizeng = Convert.ToDouble(dz.Text);
+                double date = Convert.ToDouble(shijian.Text);
+                double maichuU = Convert.ToDouble(maichu.Text);
+                double X = 0;
+                double num = 0;
+                double num1 = 0;
+                for (int a = 0; a < date; a = a + 1)
+                {
+                    if (a == 0)
+                    {
+                        num += rujin;
+                        num1 += rujin;
+                        X = rujin;
+                    }
+                    else
+                    {
+                        num1 += (num * maichuU / 100);
+                        num += X + (X * dizeng / 100);
+
+                        X = X + (X * dizeng / 100);
+                    }
+
+                }
+
+                html += @"  <li  > 总入金获得U：" + (num + num * 0.05 + num1 * 0.2).ToString("0.0000") + " </li>";
+                html += @"  <li  > 用户卖出U：" + num1.ToString("0.0000") + " </li>";
+                html += @"  <li  > 入金燃烧E获得U：" + (num * 0.05).ToString("0.0000") + " </li>";
+                html += @"  <li  > 用户出金燃烧E获得U：" + (num1 * 0.2).ToString("0.0000") + " </li>";
+                html += @"  <li  > 去掉用户卖出获得U：" + ((num + num * 0.05 + num1 * 0.2) - num1).ToString("0.0000") + " </li>";
+                html += @"  <li  > 备注：用户卖出U的计算没办法考虑用户是否出局，所以仅供参考。 </li>";
+                html += " </ol>";
+                modelshow.InnerHtml = html;
+                rj.Text = "";
+                dz.Text = "";
+                shijian.Text = "";
+                maichu.Text = "";
+                //ClientScript.RegisterStartupScript(this.GetType(), "", "<script>alert('总共获得:"+num.ToString("0.0000")+"USDT！');</script>", false);
+                //return;
+            }
+        }
+    }
+}
