eae7c16afe22801386db91b778f16153238b2b5d
niels <892182825@qq.com>
2020-12-27 12:58:02 +0800

    1

--- Web/MemberMobile/First.aspx.cs
+++ Web/MemberMobile/First.aspx.cs
@@ -54,11 +54,11 @@ public partial class Member_First : BLL.TranslationBase
             LoadMemberInfo();
 
 
-            string sql = "select COUNT(*) from memberorder where ordertype=23 and totalpv=1000";
+            string sql = "select COUNT(*) from memberorder where ordertype=23 and totalpv=1000 and DefrayType=1";
             int countdls = Convert.ToInt32(DAL.DBHelper.ExecuteScalar(sql));
-            if(countdls<290)
+            if(countdls<288)
             {
-                ClientScript.RegisterStartupScript(GetType(), "msg", "<script>alert('前300名1000U矿机还剩：" + (290- countdls) + "位！');</script>", false);
+                ClientScript.RegisterStartupScript(GetType(), "msg", "<script>alert('前300名1000U矿机还剩：" + (288- countdls) + "位！');</script>", false);
             }
             
             //lblPay.Text = Common.GetnowPrice().ToString();
